7c10dc7333ba65e085f2da32abef8033494662c0
AdarsH <adarshrawat8304@gmail.com>
2023-08-11 11:55:35 +0530

    :header.jsx

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -1,59 +1,12 @@
-import { useState } from "react";
+import React, { useState } from "react";
+
 function Header() {
-  const [isMenuOpen, setMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setMenuOpen(!isMenuOpen);
-  };
-
   return (
     <nav className="w-screen py-2 left-0 border-b border-gray-500 border-dashed">
       <div className="flex items-center justify-between p-2">
         {/* Left */}
-        <div className="ml-10 self-center text-xl whitespace-nowrap font-bold">
-          Virutal ID
-        </div>
-        {/* Middle */}
-        <div
-          className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-          id="navbar-sticky"
-        >
-          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 dark:border-gray-700 gap-14">
-            <li>
-              <a
-                href="#"
-                className="block py-2 pl-3 pr-4 rounded text-blue-500"
-                aria-current="page"
-              >
-                Home
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                About
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Contact
-              </a>
-            </li>
-          </ul>
-        </div>
-        {/* Right */}
-        <div className="flex mr-10 md:order-2">
-          <button
-            type="button"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Get started
-          </button>
+        <div className="ml-10 self-center text-2xl whitespace-nowrap font-bold">
+          <a href="/">Decorar</a>
         </div>
       </div>
     </nav>
